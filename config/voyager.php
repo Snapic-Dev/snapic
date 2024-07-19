920294bcc1b11522e4f64554017336909361ca78
JoaoSilva25 <joaosilva2505@outlook.com>
2024-07-19 03:00:33 -0300

    fix: deposit 1/2

--- config/voyager.php
+++ config/voyager.php
@@ -100,19 +100,19 @@ return [
         /*
          * Set whether or not the multilingual is supported by the BREAD input.
          */
-        'enabled' => false,
+        'enabled' => true,
 
         /*
          * Select default language
          */
-        'default' => 'en',
+        'default' => 'pt_br',
 
         /*
          * Select languages that are supported.
          */
         'locales' => [
-            'en',
-            'pt_BR',
+            //'en',
+            'pt_br',
         ],
     ],
 
@@ -144,9 +144,7 @@ return [
             ],
         ],
 
-        'widgets' => [
-
-        ],
+        'widgets' => [],
 
     ],
 
--- resources/views/elements/settings/settings-wallet.blade.php
+++ resources/views/elements/settings/settings-wallet.blade.php
@@ -1,6 +1,6 @@
 {{-- Paypal and stripe actual buttons --}}
 <div class="paymentOption paymentPP d-none">
-    <form id="wallet-deposit" method="post" action="{{route('payment.initiatePayment')}}" >
+    <form id="wallet-deposit" method="post" action="{{route('payment.initiatePayment')}}">
         @csrf
         <input type="hidden" name="amount" id="wallet-deposit-amount" value="1">
         <input type="hidden" name="transaction_type" id="payment-type" value="">
@@ -20,34 +20,36 @@
 <div>
     @include('elements/message-alert', ['classes' =>'mb-2'])
 
-    <div class="alert alert-primary text-white font-weight-bold" role="alert">
-        <div class="d-flex"><h3 class="font-weight-bold wallet-total-amount">{{\App\Providers\SettingsServiceProvider::getWebsiteFormattedAmount(number_format(Auth::user()->wallet->total, 2, '.', ''))}}</h3> <small class="ml-2"></small> </div>
-        <p class="mb-0">{{__('Available funds. You can deposit more money or become a creator to earn more.')}}</p>
+    <div class="alert alert-primary text-white font-weight-bold p-4" role="alert">
+        <div class="d-flex">
+            <h3 class="font-weight-bold wallet-total-amount">{{\App\Providers\SettingsServiceProvider::getWebsiteFormattedAmount(number_format(Auth::user()->wallet->total, 2, '.', ''))}}</h3> <small class="ml-2"></small>
+        </div>
+        <p class="mb-0 text-sm">{{__('Available funds. You can deposit more money or become a creator to earn more.')}}</p>
     </div>
 
     <div class="mt-3 inline-border-tabs">
         <nav class="nav nav-pills nav-justified">
             @foreach(\App\Providers\SettingsServiceProvider::allowWithdrawals(Auth::user()) ? ['deposit', 'withdraw'] : ['deposit'] as $tab)
-                <a class="nav-item nav-link {{$activeTab == $tab ? 'active' : ''}}" href="{{route('my.settings',['type' => 'wallet', 'active' => $tab])}}">
+            <a class="nav-item nav-link {{$activeTab == $tab ? 'active' : ''}}" href="{{route('my.settings',['type' => 'wallet', 'active' => $tab])}}">
 
-                    <div class="d-flex align-items-center justify-content-center">
-                        @if($tab == 'deposit')
-                            @include('elements.icon',['icon'=>'wallet','variant'=>'medium','classes'=>'mr-2'])
-                        @elseif(\App\Providers\SettingsServiceProvider::allowWithdrawals(Auth::user()))
-                            @include('elements.icon',['icon'=>'card','variant'=>'medium','classes'=>'mr-2'])
-                        @endif
-                        {{__(ucfirst($tab))}}
+                <div class="d-flex align-items-center justify-content-center">
+                    @if($tab == 'deposit')
+                    @include('elements.icon',['icon'=>'wallet','variant'=>'medium','classes'=>'mr-2'])
+                    @elseif(\App\Providers\SettingsServiceProvider::allowWithdrawals(Auth::user()))
+                    @include('elements.icon',['icon'=>'card','variant'=>'medium','classes'=>'mr-2'])
+                    @endif
+                    {{__(ucfirst($tab))}}
 
-                    </div>
-                </a>
+                </div>
+            </a>
             @endforeach
         </nav>
     </div>
 
     @if($activeTab != null && $activeTab === 'withdraw' && \App\Providers\SettingsServiceProvider::allowWithdrawals(Auth::user()))
-        @include('elements/settings/settings-wallet-withdraw')
+    @include('elements/settings/settings-wallet-withdraw')
     @else
-        @include('elements/settings/settings-wallet-deposit')
+    @include('elements/settings/settings-wallet-deposit')
     @endif
 
 </div>
