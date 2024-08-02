5071fadf3a90f2c175257fa2663cac6e26a0c53a
esdrasfyy <contatoesdrasoficial@gmail.com>
2024-08-01 22:43:56 -0300

    feat: upload image refact

--- public/js/FirebaseUpload.js
+++ public/js/FirebaseUpload.js
@@ -1,56 +1,44 @@
-import { storageRef } from './FirebaseConection';
+const firebaseConfig = {
+  piKey: "AIzaSyA0ztmepmqW28Lbrv1PU9E8dGnZhE0soWw",
+  authDomain: "belinho-4a703.firebaseapp.com",
+  databaseURL: "https://belinho-4a703-default-rtdb.firebaseio.com",
+  projectId: "belinho-4a703",
+  storageBucket: "belinho-4a703.appspot.com",
+  messagingSenderId: "678978718971",
+  appId: "1:678978718971:web:ba70e7d2e9031ce803745e",
+};
 
+firebase.initializeApp(firebaseConfig);
 
+// Referência ao Firebase Storage
+const storage = firebase.storage();
 
-const uploadButton = document.getElementById('uploadButton');
+export const UploadImage = async (file) => {
+  try {
+    const storageRef = ref(storage, `images/${file[0].name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file[0]);
 
-uploadButton.addEventListener('click', () => {
-//   const file = imageUpload.files[0];
-//   if (file) {
-//     uploadImage(file);
-//   } else {
-//     alert('Selecione uma imagem para fazer upload.');
-//   }
-});
-
-//upload
-function uploadImage(file) {
-  const fileRef = storageRef.child('images/' + file.name); 
-
-  fileRef.put(file)
-    .then((snapshot) => {
-      snapshot.ref.getDownloadURL()
-        .then((url) => {
-          console.log('URL de download:', url);
-        });
-
-
-    })
-    .catch((error) => {
-      console.error('Erro ao fazer upload:', error);
+    return new Promise((resolve, reject) => {
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        },
+        (error) => {
+          reject(new Error(error.message));
+        },
+        async () => {
+          try {
+            const url = await getDownloadURL(uploadTask.snapshot.ref);
+            resolve(url);
+          } catch (error) {
+            reject(new Error(error.message));
+          }
+        }
+      );
     });
-}
-
-
-const imageUpload = document.getElementById('imageUpload');
-imageUpload.addEventListener('change', () => {
-  const file = imageUpload.files[0];
-  if (file) {
-    previewImage(file); 
+  } catch (error) {
+    throw new Error(error.message);
   }
-});
-
-// Função para pré-visualizar a imagem
-function previewImage(file) {
-    const reader = new FileReader();
-    const imagePreview = document.getElementById('imagePreview');
-  
-    reader.onload = (e) => {
-      imagePreview.src = e.target.result; 
-      imagePreview.style.display = 'block'; 
-    };
-  
-    reader.readAsDataURL(file); 
-  }
-
-
+};
