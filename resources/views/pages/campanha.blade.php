c5c12d7b104476cf765a04c213d3bd9f9daa4450
JoaoSilva25 <joaosilva2505@outlook.com>
2024-08-14 18:20:28 -0300

    feat:style area de disparo 1/2

--- resources/views/pages/campanha.blade.php
+++ resources/views/pages/campanha.blade.php
@@ -16,7 +16,7 @@
         @include('elements.uploaded-file-preview-template')
         @include('elements.attachments-uploading-dialog')
         <div class="d-flex justify-content-between pt-4 pb-3 px-3 border-bottom">
-            <h5 class="text-truncate text-bold {{ Cookie::get('app_theme') == null ? (getSetting('site.default_user_theme') == 'dark' ? '' : 'text-dark-r') : (Cookie::get('app_theme') == 'dark' ? '' : 'text-dark-r') }}">
+            <h5 class="text-truncate text-bold ml-4 mr-4 p-3 {{ Cookie::get('app_theme') == null ? (getSetting('site.default_user_theme') == 'dark' ? '' : 'text-dark-r') : (Cookie::get('app_theme') == 'dark' ? '' : 'text-dark-r') }}">
                 Campanha
             </h5>
         </div>
@@ -36,16 +36,16 @@
             @endif
             <div class="d-flex flex-column-reverse">
                 <form id="myForm" class="w-100">
-                    <div class="form-group p-3">
+                    <div class="form-group p-3 ml-4 mr-4">
                         <label for="formGroupExampleInput">Valor do conteúdo</label>
-                        <input type="text" class="form-control" name="valor" id="formGroupExampleInput" placeholder="Definir valor do anúncio" required>
+                        <input type="number" class="form-control" name="valor" id="formGroupExampleInput" placeholder="Definir valor do anúncio" required>
                     </div>
-                    <div class="form-group p-3">
+                    <div class="form-group p-3 ml-4 mr-4">
                         <label for="exampleFormControlTextarea1">Mensagem</label>
                         <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Mensagem de campanha" name="message" required></textarea>
                     </div>
-                    <div class="form-group p-3">
-                        <label for="frontDoc" class="col-form-label">{{ __('Upload Documento Frente') }}</label>
+                    <div class="form-group p-3 ml-4 mr-4">
+                        <label for="frontDoc" class="col-form-label">Anexar imagem</label>
                         <input id="frontDoc" type="file" class="form-control @error('frontDoc') is-invalid @enderror" name="frontDoc" accept=".jpg, .jpeg, .png, .webp" required onchange="previewImage(this, document.getElementById('frontPreview'))">
                         <div class="preview">
                             <img id="frontPreview" src="#" alt="Preview da CNH - Frente" style="display: none; max-height: 200px;">
@@ -56,7 +56,7 @@
                         </span>
                         @enderror
                     </div>
-                    <div class="p-3">
+                    <div class="p-3 ml-4 mr-4">
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" value="1" id="defaultCheck1" name="subscribers">
                             <label class="form-check-label" for="defaultCheck1">
@@ -70,9 +70,9 @@
                             </label>
                         </div>
                     </div>
-                    <div class="d-flex justify-content-between w-100 mb-3 mt-3">
-                        <div class="form-group">
-                            <button type="button" onclick="onSubmit()" id="uploadButton" class="btn btn-primary btn-block btn-round post-create-button mb-0">{{ __('Promover') }}</button>
+                    <div class="d-flex justify-content-between w-100 mb-3 mt-3 p-3">
+                        <div class="form-group btn-block ml-4 mr-4">
+                            <button type="button" onclick="onSubmit()" id="uploadButton" class="btn btn-primary btn-block btn-round post-create-button mb-0 p-3">{{ __('Promover') }}</button>
                         </div>
                     </div>
                 </form>
