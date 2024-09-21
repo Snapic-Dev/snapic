02de124b4538d7bd1a0a055e32ac4e859a18f73c
esdrasfyy <contatoesdrasoficial@gmail.com>
2024-09-21 14:45:14 -0300

    fix: hidden buttons that need permission

--- resources/views/elements/settings/settings-menu-mobile.blade.php
+++ resources/views/elements/settings/settings-menu-mobile.blade.php
@@ -1,11 +1,15 @@
 <div class="mt-3 inline-border-tabs text-bold">
     <nav class="nav nav-pills nav-justified">
         @foreach($availableSettings as $route => $setting)
-            <a class="nav-item nav-link {{$activeSettingsTab == $route ? 'active' : ''}}" href="{{route('my.settings',['type'=>$route])}}">
-                <div class="d-flex justify-content-center">
-                    @include('elements.icon',['icon'=>$setting['icon'].'-outline','centered'=>'false','variant'=>'medium'])
-                </div>
-            </a>
+        @if(!Auth::user()->identity_verified_at || Auth::user()->role_id !== 3 && ($route==="rates" || $route==="payments"))
+        <div></div>
+        @else
+        <a class="nav-item nav-link {{$activeSettingsTab == $route ? 'active' : ''}}" href="{{route('my.settings',['type'=>$route])}}">
+            <div class="d-flex justify-content-center">
+                @include('elements.icon',['icon'=>$setting['icon'].'-outline','centered'=>'false','variant'=>'medium'])
+            </div>
+        </a>
+        @endif
         @endforeach
     </nav>
 </div>
--- resources/views/elements/settings/settings-menu.blade.php
+++ resources/views/elements/settings/settings-menu.blade.php
@@ -2,19 +2,19 @@
     <div class="card-settings border-bottom">
         <div class="list-group list-group-sm list-group-flush">
             @foreach($availableSettings as $route => $setting)
-                @if(Auth::user()->identity_verified_at===null && ($route==="rates" || $route==="payments")) 
-                <div></div>
-                @else
-                <a href="{{route('my.settings',['type'=>$route])}}" class="{{$activeSettingsTab == $route ? 'active' : ''}} list-group-item list-group-item-action d-flex justify-content-between">
-                    <div class="d-flex align-items-center">
-                        @include('elements.icon',['icon'=>$setting['icon'].'-outline','centered'=>'false','classes'=>'mr-3','variant'=>'medium'])
-                        <span>{{ucfirst(__($route))}}</span>
-                    </div>
-                    <div class="d-flex align-items-center">
-                        @include('elements.icon',['icon'=>'chevron-forward-outline'])
-                    </div>
-                </a>
-                @endif
+            @if(!Auth::user()->identity_verified_at || Auth::user()->role_id !== 3 && ($route==="rates" || $route==="payments"))
+            <div></div>
+            @else
+            <a href="{{route('my.settings',['type'=>$route])}}" class="{{$activeSettingsTab == $route ? 'active' : ''}} list-group-item list-group-item-action d-flex justify-content-between">
+                <div class="d-flex align-items-center">
+                    @include('elements.icon',['icon'=>$setting['icon'].'-outline','centered'=>'false','classes'=>'mr-3','variant'=>'medium'])
+                    <span>{{ucfirst(__($route))}}</span>
+                </div>
+                <div class="d-flex align-items-center">
+                    @include('elements.icon',['icon'=>'chevron-forward-outline'])
+                </div>
+            </a>
+            @endif
             @endforeach
         </div>
     </div>
