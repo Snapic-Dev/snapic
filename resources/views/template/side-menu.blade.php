3f9d5ba6abefa74c28b3f301f4ccf55a70bf8fc8
esdrasfyy <contatoesdrasoficial@gmail.com>
2024-09-23 16:30:53 -0300

    fix: fixed button create post for normal user

--- resources/views/template/side-menu.blade.php
+++ resources/views/template/side-menu.blade.php
@@ -343,6 +343,20 @@
         </li>
         @endif
         @endif
+        @else
+        <li class="nav-item mt-4">
+            <a role="button" class="btn btn-round border btn-primary btn-block p-3"
+                href="/my/settings/producer">
+                <span
+                    class="d-none d-md-block d-xl-block d-lg-block ml-2 text-truncate new-post-label">{{ __('Become a producer') }}</span>
+                <span
+                    class="d-block d-md-none d-flex align-items-center justify-content-center">@include('elements.icon', [
+                    'icon' => 'add-circle-outline',
+                    'variant' => 'medium',
+                    'classes' => 'flex-shrink-0',
+                    ])</span>
+            </a>
+        </li>
         @endif
     </ul>
 </div>
