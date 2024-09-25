910127e8b0b74e04054fe2d599586a2ad32076c3
Fernando Esdras da Silva <contatoesdrasoficial@gmail.com>
2024-09-25 11:25:44 -0300

    fix: logic referrals

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -770,16 +770,13 @@ class SettingsController extends Controller
         }
     }
 
-    protected function checkReferralAccess()
+  protected function checkReferralAccess()
     {
         if (!getSetting('referrals.enabled')) {
             unset($this->availableSettings['referrals']);
         }
-        if (getSetting('referrals.disable_for_non_verified')) {
-            $user = Auth::user();
-            if (!($user->email_verified_at && $user->birthdate && ($user->verification && $user->verification->status == 'verified'))) {
-                unset($this->availableSettings['referrals']);
-            }
+        if (Auth::user()->role_id !== 1 && (Auth::user()->role_id === 2 || (Auth::user()->role_id === 3 && !Auth::user()->identity_verified_at))) {
+            unset($this->availableSettings['referrals']);
         }
     }
 
