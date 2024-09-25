f4ab8ca2369a7a703f79c83880e22d9dac77397d
JoaoSilva25 <joaosilva2505@outlook.com>
2024-09-25 13:26:13 -0300

    fix: add banner page forget my password

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -68,9 +68,7 @@
                 </div>
             </div>
             <div class="col-md-6 d-none d-md-flex bg-image p-0 m-0">
-                <div class="d-flex m-0 p-0 bg-gradient-primary w-100 h-100">
-                    <img src="{{ asset('/img/pattern-lines.svg') }}" alt="pattern-lines" class="img-fluid opacity-10">
-                </div>
+                <div class="d-flex m-0 p-0 w-100 h-100 BannerLogin" style="background-image: url('{{ asset('/img/Banner.png') }}'); background-size: cover; background-position: center;"></div>
             </div>
         </div>
     </div>
