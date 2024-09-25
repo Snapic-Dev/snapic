d764cd29d8afb96e472aff07ec3fc9ac58af1aeb
Ismael dos Santos Dias <mael10774@gmail.com>
2024-09-24 23:41:59 -0300

    feat: Adicionar upload de documentos no registro de influenciadores
    Adiciona a funcionalidade de upload de documentos no registro de influenciadores. Agora, os influenciadores podem enviar os documentos de identificação frontal e traseira para verificação. Os documentos são armazenados no sistema de arquivos padrão e podem ser acessados publicamente.

--- config/filesystems.php
+++ config/filesystems.php
@@ -53,6 +53,7 @@ return [
             'region' => env('DO_SPACES_REGION'),
             'bucket' => env('DO_SPACES_BUCKET'),
             'endpoint' => env('DO_SPACES_ENDPOINT'),
+            'visibility' => 'public',
             'use_path_style_endpoint' => env('DO_USE_PATH_STYLE_ENDPOINT', false),
             'http' => [
                 'verify' => false,
