1472940bb27aa7f801cc664119d36d209544917e
esdrasfyy <contatoesdrasoficial@gmail.com>
2024-09-30 20:31:23 -0300

    fix: fixed post feed and menu producer name

--- app/Providers/PostsHelperServiceProvider.php
+++ app/Providers/PostsHelperServiceProvider.php
@@ -294,16 +294,16 @@ class PostsHelperServiceProvider extends ServiceProvider
                     return !($post->requires_subscription && !$hasSub); // Exclui posts que exigem assinatura se o usuário não for assinante
                 })
                 ->map(function ($post) use ($hasSub, $ownPosts, $data) {
-                if ($ownPosts) {
-                    $post->setAttribute('isSubbed', $hasSub);
-                } else {
-                    $post->setAttribute('isSubbed', true);
-                }
-                $post->setAttribute('postPage', $data['currentPage']);
-                $post = ['id' => $post->id, 'html' => View::make('elements.feed.post-box')->with('post', $post)->render()];
+                    if ($ownPosts) {
+                        $post->setAttribute('isSubbed', $hasSub);
+                    } else {
+                        $post->setAttribute('isSubbed', true);
+                    }
+                    $post->setAttribute('postPage', $data['currentPage']);
+                    $post = ['id' => $post->id, 'html' => View::make('elements.feed.post-box')->with('post', $post)->render()];
 
-                return $post;
-            });
+                    return $post;
+                });
             $data['posts'] = $postsData;
         } else {
             // Collection data posts | To be rendered on the server side
@@ -332,7 +332,7 @@ class PostsHelperServiceProvider extends ServiceProvider
      * @param bool $mediaType
      * @return mixed
      */
-      public static function filterPosts($posts, $userID, $filterType, $mediaType = false, $sortOrder = false, $searchTerm = '')
+    public static function filterPosts($posts, $userID, $filterType, $mediaType = false, $sortOrder = false, $searchTerm = '')
     {
         // Filtro para seguidores ou todos os posts
         // if ($filterType == 'following' || $filterType == 'all') {
@@ -354,8 +354,7 @@ class PostsHelperServiceProvider extends ServiceProvider
         if ($filterType == 'subs' || $filterType == 'all') {
             if ($filterType == 'all') {
                 // Exibe posts de assinantes ativos e perfis seguidos gratuitamente
-                $posts->where('price', 0)->where('requires_subscription', 0);
-                $posts->where('posts.user_id', '!=', $userID);
+                $posts->where('requires_subscription', 0);
             } else {
                 // Exibe apenas posts de assinantes ativos
                 $activeSubs = self::getUserActiveSubs($userID);
