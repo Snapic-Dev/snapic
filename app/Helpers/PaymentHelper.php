b3f36f85a08591f42e7fed31b16e70095cb1ea9c
JoaoSilva25 <joaosilva2505@outlook.com>
2024-10-06 17:32:21 -0300

    fix:translate tips success message

--- app/Helpers/PaymentHelper.php
+++ app/Helpers/PaymentHelper.php
@@ -1285,8 +1285,8 @@ class PaymentHelper
                     ]);
                 } elseif ($transaction->type === Transaction::TIP_TYPE || $transaction->type === Transaction::CHAT_TIP_TYPE) {
                     $key = SettingsServiceProvider::leftAlignedCurrencyPosition()
-                        ? 'You successfully sent a tip of :currencySymbol:amount.'
-                        : 'You successfully sent a tip of :amount:currencySymbol.';
+                        ? 'Você enviou com sucesso uma gorjeta de :currencySymbol:amount.'
+                        : 'Você enviou com sucesso uma gorjeta de :amount:currencySymbol.';
                     $successMessage = __($key, [
                         'amount' => $transaction->amount,
                         'currencySymbol' => SettingsServiceProvider::getWebsiteCurrencySymbol()
