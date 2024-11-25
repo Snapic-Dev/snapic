f61aef75998c7f26db32f7ce051453e9d4b32ea1
Fernando Esdras da Silva <fernaando.esdras@gmail.com>
2024-11-25 18:12:51 -0300

    fix: agreement data

--- app/Observers/TransactionsObserver.php
+++ app/Observers/TransactionsObserver.php
@@ -121,7 +121,7 @@ class TransactionsObserver
                 $data = [
                     'user_id' => $recipient->id,
                     'transaction_id' => $transaction->id,
-                    'amount' => floatval($discount_agreement - ($discount_reward ?? 0)),
+                    'amount' => floatval($discount_agreement - ($indicator ?? $discount_reward)),
                     'percentage' => (int) $recipient->discount,
                     'currency' => SettingsServiceProvider::getAppCurrencyCode(),
                 ];
