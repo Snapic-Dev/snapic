b7588a970ce13deeab47d057613696e6cf5f6590
Fernando Esdras da Silva <fernaando.esdras@gmail.com>
2024-11-27 19:00:12 -0300

    test: adjust video

--- resources/views/elements/feed/post-box-media-wrapper.blade.php
+++ resources/views/elements/feed/post-box-media-wrapper.blade.php
@@ -1,43 +1,43 @@
 @if( !(!$isGallery && AttachmentHelper::getAttachmentType($attachment->type) == 'video'))
-    <a href="{{$attachment->path}}" rel="mswp" title="">
-        @endif
+<a href="{{$attachment->path}}" rel="mswp" title="">
+    @endif
 
-        @if($isGallery)
-            @if(AttachmentHelper::getAttachmentType($attachment->type) == 'image')
-                <div class="post-media-image" style="background-image: url('{{$attachment->path}}');">
-                </div>
-            @elseif(AttachmentHelper::getAttachmentType($attachment->type) == 'video')
-                <div class="video-wrapper h-100 w-100 d-flex justify-content-center align-items-center">
-                    <video class="video-preview w-100" src="{{$attachment->path}}#t=0.001" controls controlsList="nodownload" preload="metadata" {!! ($attachment->has_thumbnail ? 'poster="'.$attachment->thumbnail.'"' : '') !!}></video>
-                </div>
-            @elseif(AttachmentHelper::getAttachmentType($attachment->type) == 'audio')
-                <div class="video-wrapper h-100 w-100 d-flex justify-content-center align-items-center">
-                    <audio class="video-preview w-75" src="{{$attachment->path}}#t=0.001" controls controlsList="nodownload" preload="metadata"></audio>
-                </div>
-            @elseif(AttachmentHelper::getAttachmentType($attachment->type) == 'long_video')
-                <div class="video-wrapper h-100 w-100 d-flex justify-content-center align-items-center object-cover">
-                    <video class="video-preview w-100 h-100" src="{{$attachment->filename}}" controls controlsList="nodownload" preload="metadata" style="min-height: 100%;object-fit: cover;" {!! ($attachment->has_thumbnail ? 'poster="'.$attachment->thumbnail.'"' : '') !!}></video>
-                </div>
-            @endif
-        @else
-            @if(AttachmentHelper::getAttachmentType($attachment->type) == 'image')
-                <img src="{{$attachment->path}}" draggable="false" alt="" class="img-fluid rounded-0 w-100">
-            @elseif(AttachmentHelper::getAttachmentType($attachment->type) == 'video')
-                <div class="video-wrapper h-100 w-100 d-flex justify-content-center align-items-center">
-                    <video class="video-preview w-100" src="{{$attachment->path}}#t=0.001" controls controlsList="nodownload" preload="metadata" {!! ($attachment->has_thumbnail ? 'poster="'.$attachment->thumbnail.'"' : '') !!}></video>
-                </div>
-            @elseif(AttachmentHelper::getAttachmentType($attachment->type) == 'audio')
-                <div class="video-wrapper h-100 w-100 d-flex justify-content-center align-items-center">
-                    <audio class="video-preview w-75" src="{{$attachment->path}}#t=0.001" controls controlsList="nodownload" preload="metadata"></audio>
-                </div>
-            @elseif(AttachmentHelper::getAttachmentType($attachment->type) == 'long_video')
-                <div class="video-wrapper h-100 w-100 d-flex justify-content-center align-items-center object-cover">
-                    <video class="video-preview w-100 h-100" src="{{$attachment->filename}}" controls controlsList="nodownload" preload="metadata" style="min-height: 100%;object-fit: cover;" {!! ($attachment->has_thumbnail ? 'poster="'.$attachment->thumbnail.'"' : '') !!}></video>
-                </div>
-            @endif
-            
-        @endif
+    @if($isGallery)
+    @if(AttachmentHelper::getAttachmentType($attachment->type) == 'image')
+    <div class="post-media-image" style="background-image: url('{{$attachment->path}}');">
+    </div>
+    @elseif(AttachmentHelper::getAttachmentType($attachment->type) == 'video')
+    <div class="video-wrapper h-100 w-100 d-flex justify-content-center align-items-center">
+        <video class="video-preview w-100" src="{{$attachment->path}}#t=0.001" controls controlsList="nodownload" preload="metadata" {!! ($attachment->has_thumbnail ? 'poster="'.$attachment->thumbnail.'"' : '') !!}></video>
+    </div>
+    @elseif(AttachmentHelper::getAttachmentType($attachment->type) == 'audio')
+    <div class="video-wrapper h-100 w-100 d-flex justify-content-center align-items-center">
+        <audio class="video-preview w-75" src="{{$attachment->path}}#t=0.001" controls controlsList="nodownload" preload="metadata"></audio>
+    </div>
+    @elseif(AttachmentHelper::getAttachmentType($attachment->type) == 'long_video')
+    <div class="video-wrapper h-100 w-100 d-flex justify-content-center align-items-center object-cover">
+        <video class="w-100 h-100" src="{{$attachment->filename}}" controls controlsList="nodownload" preload="metadata" style="min-height: 100%;object-fit: cover;" {!! ($attachment->has_thumbnail ? 'poster="'.$attachment->thumbnail.'"' : '') !!}></video>
+    </div>
+    @endif
+    @else
+    @if(AttachmentHelper::getAttachmentType($attachment->type) == 'image')
+    <img src="{{$attachment->path}}" draggable="false" alt="" class="img-fluid rounded-0 w-100">
+    @elseif(AttachmentHelper::getAttachmentType($attachment->type) == 'video')
+    <div class="video-wrapper h-100 w-100 d-flex justify-content-center align-items-center">
+        <video class="video-preview w-100" src="{{$attachment->path}}#t=0.001" controls controlsList="nodownload" preload="metadata" {!! ($attachment->has_thumbnail ? 'poster="'.$attachment->thumbnail.'"' : '') !!}></video>
+    </div>
+    @elseif(AttachmentHelper::getAttachmentType($attachment->type) == 'audio')
+    <div class="video-wrapper h-100 w-100 d-flex justify-content-center align-items-center">
+        <audio class="video-preview w-75" src="{{$attachment->path}}#t=0.001" controls controlsList="nodownload" preload="metadata"></audio>
+    </div>
+    @elseif(AttachmentHelper::getAttachmentType($attachment->type) == 'long_video')
+    <div class="video-wrapper h-100 w-100 d-flex justify-content-center align-items-center object-cover">
+        <video class="w-100 h-100" src="{{$attachment->filename}}" controls controlsList="nodownload" preload="metadata" style="min-height: 100%;object-fit: cover;" {!! ($attachment->has_thumbnail ? 'poster="'.$attachment->thumbnail.'"' : '') !!}></video>
+    </div>
+    @endif
 
-        @if( !(!$isGallery && AttachmentHelper::getAttachmentType($attachment->type) == 'video'))
-    </a>
+    @endif
+
+    @if( !(!$isGallery && AttachmentHelper::getAttachmentType($attachment->type) == 'video'))
+</a>
 @endif
