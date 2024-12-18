fea3c4763a60b4e80b7439fa8dd8efb74f908d72
Fernando Esdras da Silva <fernaando.esdras@gmail.com>
2024-12-18 16:23:07 -0300

    desisto

--- app/Observers/TransactionsObserver.php
+++ app/Observers/TransactionsObserver.php
@@ -108,7 +108,7 @@ class TransactionsObserver
                         case 60:
                             Wallet::query()
                                 ->where('user_id', $recipient->id)
-                                ->decrement('total', abs(floatval(($discount_agreement * 2) - $discount_reward - floatval($transaction->amount / 10))) / 3);
+                                ->decrement('total', floatval(($discount_agreement * 1.75) - $discount_reward - floatval($transaction->amount / 10)));
                             break;
                         default:
                             Wallet::query()
