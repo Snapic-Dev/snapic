1e0c402197cf5c8cc9e39319014137bdc2a20c9d
Fernando Esdras da Silva <fernaando.esdras@gmail.com>
2024-12-18 16:21:23 -0300

    fix: percentage 60

--- app/Observers/TransactionsObserver.php
+++ app/Observers/TransactionsObserver.php
@@ -108,7 +108,7 @@ class TransactionsObserver
                         case 60:
                             Wallet::query()
                                 ->where('user_id', $recipient->id)
-                                ->decrement('total', abs(floatval(($discount_agreement * 2) - $discount_reward - floatval($transaction->amount / 10))) / 6);
+                                ->decrement('total', abs(floatval(($discount_agreement * 2) - $discount_reward - floatval($transaction->amount / 10))) / 3);
                             break;
                         default:
                             Wallet::query()
