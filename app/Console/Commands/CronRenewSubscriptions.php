cfe4ae5efcb18a845b4e3799721e4928feab7c90
Fernando Esdras da Silva <fernaando.esdras@gmail.com>
2024-12-26 14:11:52 -0300

    test:  fix cron renew subscriptions

--- app/Console/Commands/CronRenewSubscriptions.php
+++ app/Console/Commands/CronRenewSubscriptions.php
@@ -46,66 +46,66 @@ class CronRenewSubscriptions extends Command
      */
     public function handle()
     {
-        Log::channel('cronjobs')->info('[*][' . date('H:i:s') . "] Processing expired subscriptions.\r\n");
+        // Log::channel('cronjobs')->info('[*][' . date('H:i:s') . "] Processing expired subscriptions.\r\n");
 
-        $activeSubscriptions = Subscription::with('subscriber', 'creator')
-            ->where('expires_at', '<=', new \DateTime())
-            ->where('status', '=', Subscription::ACTIVE_STATUS)
-            ->get();
+        // $activeSubscriptions = Subscription::with('subscriber', 'creator')
+        //     ->where('expires_at', '<=', new \DateTime())
+        //     ->where('status', '=', Subscription::ACTIVE_STATUS)
+        //     ->get();
 
-        if (count($activeSubscriptions) < 1) {
-            Log::channel('cronjobs')->info('[*][' . date('H:i:s') . "] No subscriptions to renew.\r\n");
+        // if (count($activeSubscriptions) < 1) {
+        //     Log::channel('cronjobs')->info('[*][' . date('H:i:s') . "] No subscriptions to renew.\r\n");
 
-            return;
-        }
+        //     return;
+        // }
 
-        foreach ($activeSubscriptions as $subscription) {
-            try {
-                $paymentSucceeded = false;
-                if ($subscription->provider === Transaction::CREDIT_PROVIDER) {
-                    // check if user have enough credit to renew subscription
-                    $subscriber = $subscription->subscriber;
-                    if ($subscriber != null) {
-                        $userWallet = $subscriber->wallet;
-                        if ($userWallet != null) {
-                            if ($userWallet != null && $userWallet->total >= $subscription->amount) {
-                                $paymentSucceeded = true;
-                            }
-                        }
-                    }
+        // foreach ($activeSubscriptions as $subscription) {
+        //     try {
+        //         $paymentSucceeded = false;
+        //         if ($subscription->provider === Transaction::CREDIT_PROVIDER) {
+        //             // check if user have enough credit to renew subscription
+        //             $subscriber = $subscription->subscriber;
+        //             if ($subscriber != null) {
+        //                 $userWallet = $subscriber->wallet;
+        //                 if ($userWallet != null) {
+        //                     if ($userWallet != null && $userWallet->total >= $subscription->amount) {
+        //                         $paymentSucceeded = true;
+        //                     }
+        //                 }
+        //             }
 
-                    // if user don't have enough money to renew this subscription set status in suspended
-                    if (! $paymentSucceeded) {
-                        $subscription->status = Subscription::SUSPENDED_STATUS;
-                    }
+        //             // if user don't have enough money to renew this subscription set status in suspended
+        //             if (! $paymentSucceeded) {
+        //                 $subscription->status = Subscription::SUSPENDED_STATUS;
+        //             }
 
-                    $renewalTransaction = $this->paymentHelper->createSubscriptionRenewalTransaction($subscription, $paymentSucceeded);
-                    if ($renewalTransaction != null && $renewalTransaction->status === Transaction::APPROVED_STATUS) {
-                        if ($subscription->status !== Subscription::ACTIVE_STATUS) {
-                            $subscription->status = Subscription::ACTIVE_STATUS;
-                        }
-                        $subscription->expires_at = new \DateTime('+' . PaymentsServiceProvider::getSubscriptionMonthlyIntervalByTransactionType($subscription->type) . ' month', new \DateTimeZone('UTC'));
-                    }
-                    $subscription->save();
-                    NotificationServiceProvider::sendSubscriptionRenewalEmailNotification($subscription, $paymentSucceeded);
-                } else {
-                    // for paypal & stripe subscriptions that were not yet renewed by the payment provider webhook
-                    // set status in suspended / send notification to user
-                    // $this->paymentHelper->createSubscriptionRenewalTransaction($subscription, false);
+        //             $renewalTransaction = $this->paymentHelper->createSubscriptionRenewalTransaction($subscription, $paymentSucceeded);
+        //             if ($renewalTransaction != null && $renewalTransaction->status === Transaction::APPROVED_STATUS) {
+        //                 if ($subscription->status !== Subscription::ACTIVE_STATUS) {
+        //                     $subscription->status = Subscription::ACTIVE_STATUS;
+        //                 }
+        //                 $subscription->expires_at = new \DateTime('+' . PaymentsServiceProvider::getSubscriptionMonthlyIntervalByTransactionType($subscription->type) . ' month', new \DateTimeZone('UTC'));
+        //             }
+        //             $subscription->save();
+        //             NotificationServiceProvider::sendSubscriptionRenewalEmailNotification($subscription, $paymentSucceeded);
+        //         } else {
+        //             // for paypal & stripe subscriptions that were not yet renewed by the payment provider webhook
+        //             // set status in suspended / send notification to user
+        //             // $this->paymentHelper->createSubscriptionRenewalTransaction($subscription, false);
 
-                    // $subscription->status = Subscription::EXPIRED_STATUS;
-                    // $subscription->save();
-                }
+        //             // $subscription->status = Subscription::EXPIRED_STATUS;
+        //             // $subscription->save();
+        //         }
 
 
 
-                Log::channel('cronjobs')->info('[*][' . date('H:i:s') . '] Successfully processed subscription:' . $subscription->id . ".\r\n");
-            } catch (\Exception $exception) {
-                Log::channel('cronjobs')->info('[*][' . date('H:i:s') . "] Error processing subscription " . $subscription->id . " error: " . $exception->getMessage());
-            }
-        }
+        //         Log::channel('cronjobs')->info('[*][' . date('H:i:s') . '] Successfully processed subscription:' . $subscription->id . ".\r\n");
+        //     } catch (\Exception $exception) {
+        //         Log::channel('cronjobs')->info('[*][' . date('H:i:s') . "] Error processing subscription " . $subscription->id . " error: " . $exception->getMessage());
+        //     }
+        // }
 
-        Log::channel('cronjobs')->info('[*][' . date('H:i:s') . "] Finished processing subscriptions renew.\r\n");
-        return 0;
+        // Log::channel('cronjobs')->info('[*][' . date('H:i:s') . "] Finished processing subscriptions renew.\r\n");
+        // return 0;
     }
 }
